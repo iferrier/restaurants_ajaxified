28a1d091ee5726fa3f7f37954603779fa483a39b
Isabelle Ferrier <ferrierisabelle@gmail.com>
2022-08-06 09:23:15 +0200

    Add seeds and index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning the DB"
+Restaurant.destroy_all
+
+puts "Creating some restaurants"
+Restaurant.create!(
+  name: "Burgermeister",
+  address: "Schlesisches Tor"
+)
+
+Restaurant.create!(
+  name: "Gazzo",
+  address: "Berlin Neukölln"
+)
+
+puts "Done!"
